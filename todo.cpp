e80fccc6404aa5dee5b7d9c4a92d706531bbe8f2
hanzala-sohrab <sorb.hanzala@gmail.com>
2021-01-01 13:26:29 +0530

    Updated help output

--- todo.cpp
+++ todo.cpp
@@ -10,12 +10,12 @@ using namespace std;
 void help()
 {
     cout << "Usage :-\n"
-             "$ ./todo add \"todo item\"  # Add a new todo\n"
-             "$ ./todo ls               # Show remaining todos\n"
-             "$ ./todo del NUMBER       # Delete a todo\n"
-             "$ ./todo done NUMBER      # Complete a todo\n"
-             "$ ./todo help             # Show usage\n"
-             "$ ./todo report           # Statistics\n";
+            "$ ./todo add \"todo item\"  # Add a new todo\n"
+            "$ ./todo ls               # Show remaining todos\n"
+            "$ ./todo del NUMBER       # Delete a todo\n"
+            "$ ./todo done NUMBER      # Complete a todo\n"
+            "$ ./todo help             # Show usage\n"
+            "$ ./todo report           # Statistics\n";
 }
 
 void missing_todo_string()
